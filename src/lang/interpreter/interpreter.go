2c61ff552ccc6f31a28e438a2eb956991b089ffb
Ankizle <ankizleyt@gmail.com>
2020-06-22 23:49:02 -0400

    fixed threads

--- src/lang/interpreter/interpreter.go
+++ src/lang/interpreter/interpreter.go
@@ -1,7 +1,10 @@
 package interpreter
 
+import "sync"
 import "reflect"
 import "unicode"
+
+var wg sync.WaitGroup
 
 func interpreter(actions []Action, cli_params CliParams, vars map[string]Variable, expReturn bool, this_vals []Action, dir string) Returner {
 
