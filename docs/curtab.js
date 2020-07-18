8b5df0bb429d746a5c8397b4848b11f68fc2a027
Ankizle <ankizleyt@gmail.com>
2020-07-18 09:32:26 -0400

    update the docs a bit

--- docs/curtab.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-  var path = window.location.pathname;
-
-  //if it is / then it must be /index.php
-  if (path == '/') path = '/index.php';
-
-  $(`a.item[href=\'${path}\']`).addClass('active');
-});
--- web/index.php
+++ web/index.php
@@ -24,7 +24,7 @@
     <link rel='stylesheet' href='styles.css'>
   </head>
 
-  <div id='navbar' class='ui inverted labeled icon large eight item menu'>
+  <div id='navbar' class='ui inverted labeled icon large nine item menu'>
 
     <a class='item menu-item' href='/index.php'>
       <i class='fas fa-home fa-2x'></i>
@@ -49,6 +49,10 @@
     <a class='item menu-item' href='/license.php'>
       <i class='fas fa-balance-scale fa-2x'></i>
       License
+    </a>
+    <a class='item menu-item' href='/docs.php'>
+      <i class='fas fa-chalkboard-teacher fa-2x'></i>
+      Tutorials
     </a>
     <a class='item menu-item' href='/docs.php'>
       <i class='fas fa-book-open fa-2x'></i>
@@ -79,45 +83,43 @@
     </div>
   </div>
 
-  <div id='feature' class='ui one center aligned page grid'>
-    <div class='ui segment inverted'>
+  <div id='feature' class='ui segment inverted'>
 
-      <h1 class='feature-head'>Compatiblility</h1>
-      <div class='ui horizontal divider'>
-        <i class='fas fa-puzzle-piece fa-2x feature-ico'></i>
-      </div>
-
-      <p class='description'>
-        Omm has an extension named OES that allows you to embed Omm scripts within other languages.
-      </p>
-
+    <h1 class='feature-head'>Object Oriented</h1>
+    <div class='ui horizontal divider'>
+      <i class='fas fa-copy fa-2x feature-ico'></i>
     </div>
 
-    <div class='ui segment inverted'>
+    <p class='description'>
+      Omm is an object oriented language, and can create a more scalable and re-usable infrastructure.
+    </p>
 
-      <h1 class='feature-head'>Precision</h1>
-      <div class='ui horizontal divider'>
-        <i class='fas fa-calculator fa-2x feature-ico'></i>
-      </div>
+  </div>
 
-      <p class='description'>
-        Omm, like python, has arbitrary precision. This means that the maximum number is limited by the machine's memory, rather than the 64 bit maxint
-      </p>
+  <div id='feature' class='ui segment inverted'>
 
+    <h1 class='feature-head'>Precision</h1>
+    <div class='ui horizontal divider'>
+      <i class='fas fa-calculator fa-2x feature-ico'></i>
     </div>
 
-    <div class='ui segment inverted'>
+    <p class='description'>
+      Omm has arbitrary integer and decimal precision. This means that Omm is not capped by the 64 bit limit, but rather the system's memory.
+    </p>
 
-      <h1 class='feature-head'>Ease of Learning</h1>
-      <div class='ui horizontal divider'>
-        <i class='fas fa-brain fa-2x feature-ico'></i>
-      </div>
+  </div>
 
-      <p class='description'>
-        Omm is a very easy language to understand, so it can be easily integrated into your project.
-      </p>
+  <div id='feature' class='ui segment inverted'>
 
+    <h1 class='feature-head'>Learning</h1>
+    <div class='ui horizontal divider'>
+      <i class='fas fa-brain fa-2x feature-ico'></i>
     </div>
+
+    <p class='description'>
+      Omm has easy and intuitive syntax which makes it easy to learn.
+    </p>
+
   </div>
 </body>
 
