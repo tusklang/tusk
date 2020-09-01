4ccc0352e309eabcf7cea6fc624aca24faf104a5
Ankizle <ankizleyt@gmail.com>
2020-09-01 03:40:40 -0400

    added custom usage message

--- omm_start.go
+++ omm_start.go
@@ -5,21 +5,19 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
-
-	. "github.com/omm-lang/omm/lang/types"
 
 	suite "github.com/omm-lang/omm-suite"
+	. "github.com/omm-lang/omm/lang/types"
+
 	"github.com/omm-lang/omm/lang/compiler"
 )
 
-var cwd = flag.String("cwd", "", "set cwd")
+var cwd = flag.String("cwd", "", "set the current working directory (automatically placed by the shell/pwsh script)")
+var ver = flag.String("ver", "", "get Omm suite version")
+var prec = flag.Uint64("prec", 20, "set the precision of an Omm instance")
 
-func defaults(cli_params *CliParams) {
-	(*cli_params).Prec = 30
-	(*cli_params).Name = ""
-	(*cli_params).Directory = ""
+func init() {
+	flag.Usage = suite.Usagef("Omm")
 }
 
 func main() {
@@ -32,10 +30,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	defaults(&cli_params)
-
 	cli_params.Directory = *cwd
 	cli_params.Name = flag.Arg(0)
+	cli_params.Prec = *prec
 
 	dirname, _ := os.Executable()
 
@@ -43,37 +40,6 @@ func main() {
 
 	//set the working directory
 	os.Chdir(cli_params.Directory)
-
-	for i := 1; i < len(flag.Args()); i++ {
-
-		v := flag.Arg(i)
-
-		if strings.HasPrefix(v, "--") {
-
-			switch v[2:] {
-
-			case "version":
-				fmt.Printf("Omm Beta %d.%d.%d", suite.OmmSuiteMajor, suite.OmmSuiteMinor, suite.OmmSuiteBug)
-				os.Exit(0)
-			}
-
-		} else if strings.HasPrefix(v, "-") {
-
-			switch v[1:] {
-
-			case "v":
-				fmt.Printf("Omm Beta %d.%d.%d", suite.OmmSuiteMajor, suite.OmmSuiteMinor, suite.OmmSuiteBug)
-				os.Exit(0)
-			case "prec":
-				tempprec, _ := strconv.ParseUint(flag.Arg(i+1), 10, 64)
-				cli_params.Prec = tempprec
-				i += 2
-			default:
-				fmt.Println("Warning, there is no cli parameter named", v)
-				i++
-			}
-		}
-	}
 
 	compiler.Run(cli_params)
 }
